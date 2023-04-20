b76383a6d62ad930ac529fe9ce9fc069b593b10b
k8pai <thek8pai@gmail.com>
2023-04-20 15:46:34 +0530

    updated card.jsx with aligning component to the center updated home.jsx with border stylings and slider overflow errors

--- src/card.jsx
+++ src/card.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React from 'react';
 import botlogo from './assets/code.png';
 
-const Card = ({name})=>{
-    return(
-        <div className="w-[250px] text my-5 shadow-md p-4 rounded-md">
-           <img className="md:w-12 w-10" src={botlogo} alt="" /> 
-           <div>
-            <h1 className="text-[#2f1793] font-bold text-xl my-3">{name}</h1>
-            <p className="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas enim dolor quibusdam earum ea.</p>
-            <a className="bg-[#2f1793] text-white rounded-md py-1 px-4" href="">Invite</a>
-           </div>
-        </div>
-    );
-}
+const Card = ({ name }) => {
+	return (
+		// updated with className property 'mx-auto' to center the div.
+		<div className="w-[250px] text my-5 shadow-md p-4 rounded-md mx-auto">
+			<img className="md:w-12 w-10" src={botlogo} alt="" />
+			<div>
+				<h1 className="text-[#2f1793] font-bold text-xl my-3">
+					{name}
+				</h1>
+				<p className="mb-3">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Voluptas enim dolor quibusdam earum ea.
+				</p>
+				<a
+					className="bg-[#2f1793] text-white rounded-md py-1 px-4"
+					href=""
+				>
+					Invite
+				</a>
+			</div>
+		</div>
+	);
+};
 
 export default Card;
